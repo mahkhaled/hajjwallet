b51294f606752515843d7605c80eede4425f5206
Ahmed El-Dakhakhny <ahmed.eldakhakhny@badrit.com>
2018-08-02 04:55:56 +0300

    editing home ui

--- android/app/src/main/java/com/hajjwallet/www/hajjwallet/fragments/ShopFragment.java
+++ android/app/src/main/java/com/hajjwallet/www/hajjwallet/fragments/ShopFragment.java
@@ -2,9 +2,16 @@ package com.hajjwallet.www.hajjwallet.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.hajjwallet.www.hajjwallet.R;
 import com.hajjwallet.www.hajjwallet.base.BaseFragment;
+
+import butterknife.ButterKnife;
 
 public class ShopFragment extends BaseFragment {
     public static ShopFragment newInstance() {
@@ -15,4 +22,13 @@ public class ShopFragment extends BaseFragment {
         fragment.setArguments(args);
         return fragment;
     }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_shop, container, false);
+
+        return view;
+
+    }
+
 }
