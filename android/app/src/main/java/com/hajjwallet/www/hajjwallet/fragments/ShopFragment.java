91d9d658897915d5d74f967dd2e81260615bfe78
Ahmed El-Dakhakhny <ahmed.eldakhakhny@badrit.com>
2018-08-03 03:53:08 +0300

    offers page

--- android/app/src/main/java/com/hajjwallet/www/hajjwallet/fragments/ShopFragment.java
+++ android/app/src/main/java/com/hajjwallet/www/hajjwallet/fragments/ShopFragment.java
@@ -1,23 +1,29 @@
 package com.hajjwallet.www.hajjwallet.fragments;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.hajjwallet.www.hajjwallet.R;
 import com.hajjwallet.www.hajjwallet.base.BaseFragment;
+import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class ShopFragment extends BaseFragment {
+    @BindView(R.id.shop_image)
+    ImageView shopImage;
+    Unbinder unbinder;
+
     public static ShopFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         ShopFragment fragment = new ShopFragment();
         fragment.setArguments(args);
         return fragment;
@@ -27,9 +33,16 @@ public class ShopFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shop, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        Picasso.get().load(R.drawable.daaa).into(shopImage);
 
         return view;
 
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
