de598153f5fba41b69edcf5c1462bc6ac7b118d9
Ahmed El-Dakhakhny <ahmed.eldakhakhny@badrit.com>
2018-08-03 07:57:49 +0300

    asda

--- merchant/app/src/main/java/com/hajjwallet/www/merchant/MerchantKeypadActivity.java
+++ merchant/app/src/main/java/com/hajjwallet/www/merchant/MerchantKeypadActivity.java
@@ -1,7 +1,15 @@
 package com.hajjwallet.www.merchant;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class MerchantKeypadActivity extends Activity {
     public static final String KEY_QR_CODE = "KEY_QR_CODE";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keypad);
+    }
 }
